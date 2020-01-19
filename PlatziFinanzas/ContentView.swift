5d4367cc04c533bf0e01b48f5b9745dd9a5e1c1d
JavierSLX <jserranolule@gmail.com>
2020-01-19 10:15:26 -0600

    Initial Commit

---
+++ PlatziFinanzas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlatziFinanzas
+//
+//  Created by Javier Serrano on 19/01/20.
+//  Copyright © 2020 Javier Serrano. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
